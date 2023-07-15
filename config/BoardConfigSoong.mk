fc8fc7dc122e314567c7b460241291cea5e9ccd1
cjh1249131356 <cjh1249131356@gmail.com>
2023-07-16 00:26:23 +0700

    soong: Add TARGET_USES_NOTHING_CAMERA
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>
    Change-Id: I2fcd12036f784d6b1f7b5d6c4cf803b72aa200c7

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -47,7 +47,8 @@ SOONG_CONFIG_lineageGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
-    uses_oplus_camera
+    uses_oplus_camera \
+    uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += lineageNvidiaVars
 SOONG_CONFIG_lineageNvidiaVars += \
@@ -72,6 +73,7 @@ SOONG_CONFIG_lineageNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWOR
 SOONG_CONFIG_lineageQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_lineageQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_lineageGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
+SOONG_CONFIG_lineageGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
