ab8cf5ab2b4cef73f97352fbbfe1cd56391ce25b
Bruno Martins <bgcngm@gmail.com>
2023-10-03 23:13:49 +0100

    config: Move TrebuchetOverlay to common_mobile
    Trebuchet is added to PRODUCT_PACKAGES in common_mobile makefile,
    so place it there because it's useless to include it on ATV targets.
    
    Change-Id: Ia6235f6ac0eede86e2fd8979c50bda3fa2e06615

--- config/common.mk
+++ config/common.mk
@@ -207,8 +207,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
-    NetworkStackOverlay \
-    TrebuchetOverlay
+    NetworkStackOverlay
 
 # Translations
 CUSTOM_LOCALES += \
