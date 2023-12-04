bananadroid/android_vendor_banana

config: Copy Lineage health & livedisplay permissions
Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd

config/common.mk
@@ -46,6 +46,11 @@ PRODUCT_COPY_FILES += \
 $(foreach f,$(wildcard vendor/banana/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
 
+# Permissions for lineage sdk services
+PRODUCT_COPY_FILES += \
+    vendor/banana/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
+    vendor/banana/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.livedisplay.xml
+
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
     vendor/banana/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.nfc.beam.xml

LineVall/linevall_vendor_linevall

Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

config/common.mk
@@ -5,6 +5,11 @@ PRODUCT_BRAND ?= LineVallOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+# Permissions for lineage sdk services
+PRODUCT_COPY_FILES += \
+    vendor/linevall/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
+    vendor/linevall/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.livedisplay.xml
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml