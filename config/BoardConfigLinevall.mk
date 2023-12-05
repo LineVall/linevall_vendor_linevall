c56b6601bdabf991c85fc4207d37d43481f95d82
Henrique Pereira <hlcpereira@pixelexperience.org>
2023-12-05 09:52:43 +0700

    vendor: Add fwk-detect namespace
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Change-Id: I826946e5e7a3e951ceb699ed3b5791813be9ae5b
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- config/BoardConfigLinevall.mk
+++ config/BoardConfigLinevall.mk
@@ -5,3 +5,8 @@ include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/linevall/config/BoardConfigSoong.mk
+
+# Namespace for fwk-detect
+TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
+PRODUCT_SOONG_NAMESPACES += \
+    $(TARGET_FWK_DETECT_PATH)/fwk-detect
