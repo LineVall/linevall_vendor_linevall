ae0eccab852f3090a1ecf49741e88b93474d3e51
unsatifsed27 <unsatifsederror@gmail.com>
2025-05-12 16:22:01 +0700

    snsnwksj
    Change-Id: Icd133a83b7d2c1e9ba157a5da9980735aa81dca4

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -23,6 +23,9 @@ ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(warning TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED is deprecated, please migrate to soong_config_set,camera,override_format_from_reserved)
     $(call soong_config_set,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
 endif
+ifneq ($(TARGET_CAMERA_PACKAGE_NAME),)
+    $(call soong_config_set,camera,package_name,$(TARGET_CAMERA_PACKAGE_NAME))
+endif
 ifneq ($(TARGET_CAMERA_SERVICE_EXT_LIB),)
     $(call soong_config_set,libcameraservice,ext_lib,$(TARGET_CAMERA_SERVICE_EXT_LIB))
 endif
